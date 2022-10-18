b798885a088828d4402252d19d1fd7e46e7d7201
Adam Ralph <adamralph@posteo.net>
2022-10-19 10:27:26 +1100

    login progress

--- js/login.js
+++ js/login.js
@@ -1,6 +1,17 @@
-const login = document.querySelector(".form-signin");
+const login = document.getElementById("form-signin");
 
 login.addEventListener("submit", (e) => {
-  window.location.href = "home.html";
+  // window.location.href = "home.html";
+  const username = document.querySelector(".username").value;
+  const password = document.querySelector(".password").value;
+  const data = new FormData();
+  console.log(username);
+  console.log(password);
+  if (username === "admin" && password === "admin") {
+    localStorage.setItem((admin = "loggedIn"));
+  }
+  // for (const value of data.values()) {
+  //   console.log(value);
+  // }
   e.preventDefault();
 });
