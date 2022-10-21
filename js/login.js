15d0f6271768216795fea5b8b20f6585f640ded3
Adam Ralph <adamralph@posteo.net>
2022-10-21 11:01:24 +1100

    loading spinner working on sign in

--- js/login.js
+++ js/login.js
@@ -3,21 +3,28 @@ const admin = "";
 
 // localStorage.removeItem("admin");
 
-login.addEventListener("submit", (e) => {
-  // window.location.href = "home.html";
+login.addEventListener("submit", async (e) => {
   const username = document.querySelector(".username").value;
   const password = document.querySelector(".password").value;
   console.log(username);
   console.log(password);
   if (username === "admin" && password === "admin") {
+    await signInDelay();
     localStorage.setItem("admin", 1);
-
-    setTimeout(() => {
-      window.location.href = "home.html";
-    }, 2000);
+    activateLoadingSpinner();
   }
-  // for (const value of data.values()) {
-  //   console.log(value);
-  // }
   e.preventDefault();
 });
+
+function activateLoadingSpinner() {
+  let btn = document.getElementById("sign-in-btn");
+  let spinner = document.getElementById("sign-in-spinner");
+  btn.style.display = "none";
+  spinner.style.display = "block";
+}
+
+function signInDelay() {
+  setTimeout(() => {
+    window.location.href = "home.html";
+  }, 2000);
+}
