19d0c7df464ed20dbfd078173c91303ab834bb9f
Adam Ralph <adamralph@posteo.net>
2022-11-22 10:44:15 +1100

    updates

--- training/one/one.js
+++ training/one/one.js
@@ -4,3 +4,26 @@ function setTrainingToken() {
     localStorage.removeItem("admin")
 }
 
+const question = document.getElementById("question")
+const log = document.getElementById("log")
+
+question.addEventListener(
+    "submit",
+    (e) => {
+        const data = new FormData(question)
+        let questionNum = question.dataset.number
+        let output = ""
+        for (const entry of data) {
+            if (entry[1] === "correct") {
+                localStorage.setItem(questionNum, 1)
+                output = `That is the correct answer!`
+            } else {
+                localStorage.setItem(questionNum, 0)
+                output = "Sorry, that is not the right answer"
+            }
+        }
+        log.innerText = output
+        e.preventDefault()
+    },
+    false
+)
