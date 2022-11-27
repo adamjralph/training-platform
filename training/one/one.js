921927835631a2cd70bc9c54980bbf175c8cb2d8
Adam Ralph <adamralph@posteo.net>
2022-11-27 18:38:25 +1100

    updates

--- training/one/one.js
+++ training/one/one.js
@@ -1,8 +1,8 @@
-function setTrainingToken() {
-    localStorage.setItem("isTraining", 1)
-    // When training commences the admin is logged out to prevent student from accessing admin area
-    localStorage.removeItem("admin")
-}
+// function setTrainingToken() {
+//     localStorage.setItem("isTraining", 1)
+//     // When training commences the admin is logged out to prevent student from accessing admin area
+//     localStorage.removeItem("admin")
+// }
 
 
 
